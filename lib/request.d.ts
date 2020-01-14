7598aa67414768c92efb0f0146c6170460e1b775
stk-dmitry <stkdmitry@yandex.ru>
2020-01-14 23:25:02 +0300

    upd

--- lib/request.d.ts
+++
@@ -1,8 +0,0 @@
-import { Dispatch } from 'redux';
-import { IDefaultOption, IRequestOptions } from './definition';
-export default class requestManager {
-    requestObject: object;
-    private options;
-    constructor(requestObject: object, options: IDefaultOption);
-    send: (rq: IRequestOptions) => (dispatch: Dispatch<import("redux").AnyAction>, getState: () => any) => Promise<void>;
-}
