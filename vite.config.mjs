922e6671b33c41127f622bfdc85286e037801599
senelway <senelway@gmail.com>
2024-12-24 12:12:52 +0000

    feat: add types in lib

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,13 @@
+/// <reference types="vite/client" />
+
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  plugins: [
+    dts({ include: ['lib'] }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/index.ts'),
